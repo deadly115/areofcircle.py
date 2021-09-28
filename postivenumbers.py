1b4bdb81e000add980bceb5bc2568dff81d7ae3c
deadly115 <79560120+deadly115@users.noreply.github.com>
2021-09-28 21:43:55 +0530

    Update postivenumbers.py

--- postivenumbers.py
+++ postivenumbers.py
@@ -3,10 +3,12 @@
 Created on Tue Sep 28 21:36:10 2021
 
 @author: 91913
-"""
-
-
+""
 start, end = -4, 19
-    for num in range(start, end + 1)
-      if num >= 0:
+ 
+
+for num in range(start, end + 1):
+     
+    
+    if num >= 0:
         print(num, end = " ")
